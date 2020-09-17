9748a7099bbf93b73e9abdbdabc061ab5e8bcb22
Mukhtar Janagale <janogale@users.noreply.github.com>
2020-09-17 22:33:55 +0300

    day 2

---
+++ day1/day1.js
@@ -0,0 +1,9 @@
+let h1 = document.querySelector("h1");
+h1.style.color = "#090";
+
+h1.addEventListener("click", () => {
+  console.log("HTML and JS are connected");
+  document.body.style.background = "#222";
+});
+
+//alert("Bismilaah");
---
+++ day2/script.js
@@ -0,0 +1,76 @@
+/*
+hide h1
+and button is clicked
+ */
+
+btn.addEventListener("click", function () {
+  console.log("clicked");
+  title.style.display = "none";
+});
+
+// very if js is connected to html
+console.log("connected");
+
+// Variables
+
+let country;
+
+// declare variable and assign avalue.
+
+let myName = "Mukhtar";
+
+// let vs var
+
+// 2009 ES5 - EcmaScript 5
+// var   old
+
+//ES6 2015 let, const
+
+// change variable value
+myName = "Jaamac";
+
+console.log(myName);
+
+// const variable
+
+// let student = prompt("what is your name?");
+
+// let greeting = "Iska Waran ";
+
+// alert(greeting + student);
+
+const year = 2020;
+
+// value con not be change if const keyword is used
+//year = 2021;
+console.log(year);
+
+let test = "test";
+
+// data types
+
+// dynamic typing   - javascript
+// static typing
+2 * "hello ";
+
+// typeof
+
+console.log(typeof year);
+
+// string type  - 3 ways
+
+let course = "Javascript";
+let courseType = "Remote";
+let school = `Elmi Academy @ ${year}`;
+
+console.log(course);
+course = null;
+
+console.log(course);
+console.log(school);
+
+
+
+let myVariable;
+
+
